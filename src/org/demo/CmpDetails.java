7316fe872f5c122c092271da728fda68891da93b
vasukikumaresan <vasukikumaresan1211gmail.com>
2025-03-31 12:52:49 +0530

    my first code push

---
+++ src/org/demo/CmpDetails.java
@@ -0,0 +1,36 @@
+package org.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CmpDetails {
+
+	public static void main(String[] args) {
+		
+		
+		//Interface refname= new ClassName();
+		
+		List l=new ArrayList();
+		
+		l.add(10);
+		l.add(1234.67f);
+		l.add("Java");
+		l.add('M');
+		l.add(987654321);
+		l.add(10);
+		
+		System.out.println(l);
+		
+		//To find the size of the list
+		int a = l.size();
+		System.out.println("Size of the list is:"+a);
+		
+		//To get the particular value from the list using index position
+		Object b = l.get(5);
+		System.out.println(b);
+		
+		
+	}
+	
+	
+}
---
+++ src/org/demo/Company.java
@@ -0,0 +1,34 @@
+package org.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+	
+	public static void main(String[] args) {
+		
+		// interface refname = new Classname();
+		
+		List<Integer> l= new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
+		l.add(50);
+		
+		System.out.println(l);
+		
+		// To find the size of the list
+		int a = l.size();
+		System.out.println(a);
+		
+		
+		// To find the particular value from the list
+	
+		
+		
+		
+	}
+	
+
+}
